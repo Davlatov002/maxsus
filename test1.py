405f0ad33c5ef656f9e6ce9494c6b1b048cf966b
Davlatov Barot <davlatovbarot@Davlatovs-MacBook-Pro.local>
2023-11-17 16:24:36 +0500

    new

--- test1.py
+++ test1.py
@@ -6,17 +6,17 @@ def get_bnb_price(address):
     # url = f"https://api.bscscan.com/api?module=account&action=balance&address={address}&tag=latest"
 
     # for _ in range(retries):
-    try:
-        url = f"https://api.bscscan.com/api?module=account&action=balance&address={address}&tag=latest"
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an HTTPError for bad responses
-        data = response.json()
-        balance_in_wei = int(data['result'])
-        bnb_price_in_wei = 10 ** 18  # 1 BNB in Wei
-        balance_in_bnb = balance_in_wei / bnb_price_in_wei
-        return balance_in_bnb
-    except ConnectionError as e:
-        print(f"Connection error: {e}")
+    # try:
+    url = f"https://api.bscscan.com/api?module=account&action=balance&address={address}&tag=latest"
+    response = requests.get(url)
+    response.raise_for_status()  # Raise an HTTPError for bad responses
+    data = response.json()
+    balance_in_wei = int(data['result'])
+    bnb_price_in_wei = 10 ** 18  # 1 BNB in Wei
+    balance_in_bnb = balance_in_wei / bnb_price_in_wei
+    return balance_in_bnb
+    # except ConnectionError as e:
+    #     print(f"Connection error: {e}")
         
         # Introduce a delay before retrying
         
